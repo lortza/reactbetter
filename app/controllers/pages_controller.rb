35661903fce0ab36ba001c2e0015e4df4e7e87ae
anne <richardson.ae@gmail.com>
2016-01-11 20:45:09 -0600

    basic site design in place

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,12 @@
 class PagesController < ApplicationController
+  
   def home
-    
   end #home
 
   def about
   end
 
   def react
-
     @reaction = Reaction.published.sample
   end #react
 end
