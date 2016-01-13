62c59f5734c70fb6333d47ab2e861ebb4eccadf3
anne <richardson.ae@gmail.com>
2016-01-12 20:04:54 -0600

    new design for reactions index

--- app/helpers/reactions_helper.rb
+++ app/helpers/reactions_helper.rb
@@ -6,4 +6,12 @@ module ReactionsHelper
     end #if
   end #display_unpublished_indicator
 
+  def publish_status_style(reaction)
+    if reaction.publish_status == "Published"
+      "listing"
+    else
+      "listing unpublished-article"
+    end #if
+  end #publish_status_style     
+
 end #ReactionsHelper
