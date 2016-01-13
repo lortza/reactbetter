173174475186ac88b008e06c3c02208a7fcb1bca
anne <richardson.ae@gmail.com>
2016-01-12 23:13:42 -0600

    removed commented out code

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,8 +1,3 @@
 module PagesHelper
-  # def display_home_image
-  #   lemon_images = ["citrus-991090_1920.jpg", "lemon-681134_1920.jpg", "lemon-682698_1920.jpg", "lemon-684203_1920.jpg", "lemon-1063977_1920.jpg", "lemon-1117565_1920.jpg", "lemons-811419_1920.jpg", "lemons-812069_1920.jpg"]  
-  #   lemonade_images = ["background-906143_1920.jpg", "background-906145_1920.jpg", "beverage-1030150_1920.jpg", "lemon-933210_1920.jpg", "lemon-squeezer-609273_1920.jpg", "passion-fruit-daiquiri-906099_1920.jpg", "pink-lemonade-795030_1920.jpg", "lemon-1024641_1920.jpg", "mason-jar-691342_1280.jpg", "bowl-336538_1920.jpg", "tea-556777_1920.jpg"]
-  #   lemon_images.sample 
-  # end #display_home_image
     
 end #PagesHelper
