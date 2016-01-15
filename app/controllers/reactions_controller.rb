f9b6d0562cff27553017a57bdfe85e1ae5b13991
anne <richardson.ae@gmail.com>
2016-01-14 22:05:37 -0600

    confirmation notices and redirect update back to index

--- app/controllers/reactions_controller.rb
+++ app/controllers/reactions_controller.rb
@@ -31,7 +31,7 @@ class ReactionsController < ApplicationController
 
     respond_to do |format|
       if @reaction.save
-        format.html { redirect_to @reaction, notice: 'Reaction was successfully created.' }
+        format.html { redirect_to @reaction, notice: @reaction.name + ' was successfully created. Nice work!' }
         format.json { render :show, status: :created, location: @reaction }
       else
         format.html { render :new }
@@ -45,7 +45,7 @@ class ReactionsController < ApplicationController
   def update
     respond_to do |format|
       if @reaction.update(reaction_params)
-        format.html { redirect_to @reaction, notice: 'Reaction was successfully updated.' }
+        format.html { redirect_to reactions_url, notice: @reaction.name + ' was successfully updated.' }
         format.json { render :show, status: :ok, location: @reaction }
       else
         format.html { render :edit }
@@ -59,7 +59,7 @@ class ReactionsController < ApplicationController
   def destroy
     @reaction.destroy
     respond_to do |format|
-      format.html { redirect_to reactions_url, notice: 'Reaction was successfully destroyed.' }
+      format.html { redirect_to reactions_url, notice: 'Poof! ' + @reaction.name + ' was successfully deleted.' }
       format.json { head :no_content }
     end
   end
