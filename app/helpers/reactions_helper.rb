e8458e6e4b368ce97bbba3c285e7f2bba1a7bfc2
anne <richardson.ae@gmail.com>
2016-01-14 21:51:25 -0600

    Reaction show page design and helpers

--- app/helpers/reactions_helper.rb
+++ app/helpers/reactions_helper.rb
@@ -2,7 +2,7 @@ module ReactionsHelper
 
   def display_unpublished_indicator(reaction)
     if reaction.publish_status == "Unpublished"
-      "(Unpublished)"
+      "Unpublished"
     end #if
   end #display_unpublished_indicator
 
